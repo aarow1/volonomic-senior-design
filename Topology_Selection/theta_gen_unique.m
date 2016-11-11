85f0568ad4086bd7c810a0cce4f4a1789194eb6a
bbcourt13 <cokobata@seas.upenn.edu>
2016-11-11 18:34:19 -0500

    Update theta_gen_unique.m

--- Topology_Selection/theta_gen_unique.m
+++ Topology_Selection/theta_gen_unique.m
@@ -10,7 +10,7 @@ rd(3) = n*(n-1)           *2;     % 3 2
 rd(4) = n*nchoosek(n-1,2) *2;     % 3 1 1
 rd(5) = n*nchoosek(n-1,2) *4;     % 2 2 1
 rd(6) = n*nchoosek(n-1,3) *6;     % 2 1 1 1
-rd(7)=nchoosek(n,5)*factorial(4); % 1 1 1 1 1
+rd(7)=nchoosek(n,5)*factorial(4)/2; % 1 1 1 1 1
 
 tot = sum(rd);
 
@@ -28,7 +28,7 @@ for i = 1:rd(2)/2
     theta2(even,:) = [b*[1 1 1 1], a];
 end
 
-%% choose 3 and 2
+%% theta3 -- choose 3 and 2
 for i = 1:rd(3)/4 % a a a b b
     odd = 2*i-1;
     even = 2*i;
@@ -38,8 +38,8 @@ for i = 1:rd(3)/4 % a a a b b
     theta3(even,:) = [b*[1 1 1], a*[1 1]];
 end
 for i = 1:rd(3)/4 % a a b a b
-    odd = 132 + 2*i-1;
-    even = 132 + 2*i;
+    odd = rd(3)/2 + 2*i-1;
+    even = rd(3)/2 + 2*i;
     a = v(c_2(i,1));
     b = v(c_2(i,2));
     theta3(odd,:) = [a, a, b, a, b];
@@ -59,8 +59,8 @@ for i = 1:n
         b = opts(c11_2(j,1));
         c = opts(c11_2(j,2));
         
-        odd = (i-1)*110 + 2*j-1;
-        even = (i-1)*110 + 2*j;
+        odd = (i-1)*num*2 + 2*j-1;
+        even = (i-1)*num*2 + 2*j;
         theta4(odd,:) = [a, a, a, b, c];
         theta4(even,:) = [a, a, b, a, c];
     end
@@ -76,10 +76,10 @@ for i = 1:n
         b = opts(c11_2(j,1));
         c = opts(c11_2(j,2));
         
-        g_1 = (i-1)*220 + 4*j-3;
-        g_2 = (i-1)*220 + 4*j-2;
-        g_3 = (i-1)*220 + 4*j-1;
-        g_4 = (i-1)*220 + 4*j;
+        g_1 = (i-1)*num*4 + 4*j-3;
+        g_2 = (i-1)*num*4 + 4*j-2;
+        g_3 = (i-1)*num*4 + 4*j-1;
+        g_4 = (i-1)*num*4 + 4*j;
         
         theta5(g_1,:) = [a b c c b];
         theta5(g_2,:) = [a c b b c];
@@ -96,17 +96,18 @@ for i = 1:n
     opts = v;
     a = v(i);
     opts(opts == a) = [];
+    k = rd(6)/n;
     for j = 1:num3
         b = opts(c11_3(j,1));
         c = opts(c11_3(j,2));
         d = opts(c11_3(j,3));
-
-        g_1 = (i-1)*990 + 6*j-5;
-        g_2 = (i-1)*990 + 6*j-4;
-        g_3 = (i-1)*990 + 6*j-3;
-        g_4 = (i-1)*990 + 6*j-2;
-        g_5 = (i-1)*990 + 6*j-1;
-        g_6 = (i-1)*990 + 6*j;
+        
+        g_1 = (i-1)*k + 6*j-5;
+        g_2 = (i-1)*k + 6*j-4;
+        g_3 = (i-1)*k + 6*j-3;
+        g_4 = (i-1)*k + 6*j-2;
+        g_5 = (i-1)*k + 6*j-1;
+        g_6 = (i-1)*k + 6*j;
 
         theta6(g_1,:) = [a a b c d];
         theta6(g_2,:) = [a a b d c];
@@ -117,18 +118,44 @@ for i = 1:n
     end
 end
 
-% %% choose 1 1 1 1 1
-% c12_5 = nchoosek([1:12],5);
-% num5 = nchoosek(12,5);
-% for i = 1:length(num5)
-%     a = num5(i,1);
-%     b = num5(i,2);
-%     c = num5(i,3);
-%     d = num5(i,4);
-%     e = num5(i,5);
-% 
-% end
+%% choose 1 1 1 1 1
+c12_5 = nchoosek([1:n],5);
+num5 = nchoosek(n,5);
+theta7 = zeros([rd(7) 5]);
+for i = 1:length(c12_5)
+    a = v(c12_5(i,1));
+    b = v(c12_5(i,2));
+    c = v(c12_5(i,3));
+    d = v(c12_5(i,4));
+    e = v(c12_5(i,5));
+    
+    g_1 = (i-1)*12 + 1;
+    g_2 = (i-1)*12 + 2;
+    g_3 = (i-1)*12 + 3;
+    g_4 = (i-1)*12 + 4;
+    g_5 = (i-1)*12 + 5;
+    g_6 = (i-1)*12 + 6;
+    g_7 = (i-1)*12 + 7;
+    g_8 = (i-1)*12 + 8;
+    g_9 = (i-1)*12 + 9;
+    g_10 = (i-1)*12 + 10;
+    g_11 = (i-1)*12 + 11;
+    g_12 = (i-1)*12 + 12;
+    
+    theta7(g_1,:) = [a b c d e];
+    theta7(g_2,:) = [a b c e d];
+    theta7(g_3,:) = [a b d c e];
+    theta7(g_4,:) = [a b d e c];
+    theta7(g_5,:) = [a b e c d];
+    theta7(g_6,:) = [a b e d c];
+    theta7(g_7,:) = [a c d b e];
+    theta7(g_8,:) = [a c d e b];
+    theta7(g_9,:) = [a c e b d];
+    theta7(g_10,:) = [a c e d b];
+    theta7(g_11,:) = [a d e c b];
+    theta7(g_12,:) = [a d e b c];
+end
 
 %% Total angle
-theta = [theta1(:,:); theta2(:,:); theta3(:,:); theta4(:,:); theta5(:,:); theta6(:,:)];
+theta = [theta1(:,:); theta2(:,:); theta3(:,:); theta4(:,:); theta5(:,:); theta6(:,:); theta7(:,:)];
 end
