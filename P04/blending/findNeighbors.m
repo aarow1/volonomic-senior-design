738521eb28d1d0c4fe0eb419ccf972d1fab02280
Cheryl Liu <chl@seas.upenn.edu>
2016-11-21 02:50:57 -0500

    Update findNeighbors.m
    I'm an idiot

--- P04/blending/findNeighbors.m
+++ P04/blending/findNeighbors.m
@@ -1,16 +1,18 @@
 function [ neighborIdx ] = findNeighbors(size, idx)
-[y,x] = ind2sub(size(indexes),idx);
-[m,n] = size(indexes);
+%[y,x] = ind2sub(size(indexes),idx);
+n = size(2);
 neighborIdx = zeros(4);
-
-for k = 1:4
-    try
+neighborIdx(1) = idx+1;
+neighborIdx(2) = idx-1;
+neighborIdx(3) = idx+n;
+neighborIdx(4) = idx-n;
+%for k = 1:4
+ %   try
         %         y = real((1i)^k);
         %         x = real((1i)^(k+1));
-        neighborIdx(k) = sub2ind([m,n],x+real((1i)^(k+1)),y+real((1i)^k));
-    catch
-        neighborIdx(k) = idx;
-    end
+    %    neighborIdx(k) = sub2ind([m,n],x+real((1i)^(k+1)),y+real((1i)^k));
+ %   catch
+    %    neighborIdx(k) = idx;
+%    end
+%end
 end
-end
-
