cf6495cca54d1c7f3f5efe8d48715b45bdde9a4e
bbcourt13 <cokobata@seas.upenn.edu>
2016-11-28 21:27:32 -0500

    Update runBlending.m
    fixed dimensions

--- P04/blending/runBlending.m
+++ P04/blending/runBlending.m
@@ -3,11 +3,13 @@ clear all;
 %read in images
 sourceImg = imread('source.jpg');
 targetImg = imread('target.jpg');
-[tm,tn] = size(targetImg);
-targetImg = targetImg(1:tm-(mod(tm,2)),1:tn-(mod(tn,2)));
-[tm,tn] = size(targetImg);    
-[sm,sn] = size(sourceImg);
-souceImg = padarray(sourceImg,[(tm-sm)/2 (tn-sn)/2 0]);
+[tm,tn,~] = size(targetImg);
+targetImg = targetImg(1:tm-(mod(tm,2)),1:tn-(mod(tn,2)),:);
+[sm,sn,~] = size(sourceImg);
+sourceImg = sourceImg(1:sm-(mod(sm,2)),1:sn-(mod(sn,2)),:);
+[tm,tn,~] = size(targetImg);    
+[sm,sn,~] = size(sourceImg);
+sourceImg = padarray(sourceImg,[(tm-sm)/2 (tn-sn)/2 0]);
 
 %create mask
 [mask] = maskImage(sourceImg);
