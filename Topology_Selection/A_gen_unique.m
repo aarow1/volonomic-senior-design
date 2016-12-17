22481a4aeb1d68b0dd594a90b2bf4627bb4768b9
cherylliu <chl@seas.upenn.edu>
2016-12-16 21:06:09 -0500

    prop characterization pics

--- Topology_Selection/A_gen_unique.m
+++ Topology_Selection/A_gen_unique.m
@@ -3,19 +3,19 @@
 % tic;
 % display('----START-----');
 % display(['timestamp: ' datestr(now, 'HH:MM:SS')]);
-function A = A_gen_unique(~, step)
-
+% function A = A_gen_unique(~, A_step)
+A_step = pi/12;
 thetaList = theta_gen_unique(A_step);
-for i = 1:length(thetasDone)
-    thetaList = setdiff(thetaList,theta_gen_unique(thetasDone(i)),'rows');
-end
+% for i = 1:length(thetasDone)
+%     thetaList = setdiff(thetaList,theta_gen_unique(thetasDone(i)),'rows');
+% end
 
 y = 'y';
 z = 'z';
 
 index = 1;
-stepEnd = 2*pi-step;
-%theta = theta_gen_unique(pi/12);
+stepEnd = 2*pi-A_step;
+% theta = theta_gen_unique(pi/12);
 % modsplit = 2000;
 % A = zeros(6,7,length(theta));
 startVal = 1;
@@ -27,8 +27,8 @@ for i = startVal:endVal;
     t_3 = thetaList(i,3);
     t_4 = thetaList(i,4);
     t_5 = thetaList(i,5);
-    for t_6 = 0:step:stepEnd
-        for t_7 = 0:step:stepEnd
+    for t_6 = 0:A_step:stepEnd
+        for t_7 = 0:A_step:stepEnd
 %              index = (2*pi/step)^2*(i-1)+(2/step)*t_6*(pi/step)+t_7/step+1;
             F = [Rot3D(z,0)*Rot3D(y,t_1)*[0 0 1 1]' ...
                 Rot3D(z,2*pi/5)*Rot3D(y,t_2)*[0 0 1 1]'...
