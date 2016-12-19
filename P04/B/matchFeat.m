8c8d63bc67aea94b5821647be387cb38c3b4c69f
cherylliu <chl@seas.upenn.edu>
2016-12-18 23:43:38 -0500

    found SIFT code

--- P04/B/matchFeat.m
+++ P04/B/matchFeat.m
@@ -1,25 +1,23 @@
-function [ idxList ] = matchFeat( featurePts,img )
-imgGray = double(padarray(rgb2gray(img),[8 8]));
+function [ match] = matchFeat( tileBins1,tileBins2 )
+%input: tileBins1- 128 x N1
+% match is n1x1 vector of integers where m(i) points to the index of the
+% descriptor in p2 that matches with the descriptor p1(:,i).
+% If no match is found, m(i) = -1
+tileBins1 = tileBins1';
+tileBins2 = tileBins2';
+thresh = .8;
+[m,n] = size(tileBins1);
 
-[m,n] = size(featurePts);
-winSz = 16;
-for i = 1:n
-    locPts = double(featurePts{1,i}.Location);
-    for j = 1:length(locPts)
-        window = img(locPts(i,1):locPts(i,1)+winSz-1,locPts(i,2):locPts(i,2)+winSz-1);
-        [mag{i,j}, dir{i,j}] = imgradient(window);
-        magMat = cell2mat(mag{i,j}); dirMat = cell2mat(dir{i,j});
-        thresh = max(mag{i,j})/3git;
-        thresh = 0;
-        filterMat = dirMat(abs(magMat)>thresh);
-        filterMat = deg2rad(filterMat);
-        bounds = -pi:pi/4:pi;
-        y = discretize(filterMat, bounds);
-        discMat = pi/4*y;
-        
-        histCells{i,j} = discMat;
-    end 
+idx = knnsearch(tileBins1,tileBins2,'K',2);
+match = zeros(m,1);
+for i = 1:m
+    ssd1 = ssd(tileBins2(i,:),tileBins1(idx(i,1),:));
+    ssd2 = ssd(tileBins2(i,:),tileBins1(idx(i,2),:));
+    if (ssd1/ssd2 <= thresh)
+        match(i) = idx(i,1);
+    else
+        match(i) = -1;
+    end
 end
-
 
 end
