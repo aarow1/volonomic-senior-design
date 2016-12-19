58d3bce0c31561e5a519b6640fff7b88e47f21cf
bbcourt13 <cokobata@seas.upenn.edu>
2016-12-19 10:18:37 -0500

    Add files via upload

--- P04/B/faceSwapWrapper.m
+++ P04/B/faceSwapWrapper.m
@@ -3,6 +3,7 @@ function img_frame = faceSwapWrapper(img)
 % clear all;
 %
 % img = imread('Capture.png');
+try
 disp('FINDING FEATURES');
 boundBox = [detectFace(img,[600, 60, 400, 600]); detectFace(img,[1800, 60, 500, 600])];
 showAll = 1;
@@ -20,26 +21,26 @@ boxEyeL = findFeat(img,boundBox+[boundBox(2,4)/2 -boundBox(1,3)/4 0 0;...
 % boxEyeR = findFeat(img,boundBox+[ 0 0 0 0;  0 -60 0 0;],'RightEyeCART',1);
 % boxEyeL = findFeat(img,boundBox+[ 0 0 0 0;  50 -40 0 0;],'LeftEyeCART',1);
 
-[faceShapeX,faceShapeY] = findCoorPts(boxNose, boxMouth, boxEyeR, boxEyeL);
-
-if showAll
-    figure(); imshow(img);
-    hold on;
-    scatter(faceShapeX(:), faceShapeY(:));
-    hold off;
-end
-convHullX = faceShapeX(:,1:4);
-convHullY = faceShapeY(:,1:4);
-
-img_mask1 = makeMask(img,convHullX(1,:),convHullY(1,:));
-img_mask2 = makeMask(img,convHullX(2,:),convHullY(2,:));
-
-disp('MORPHING IMAGE');
-try
-img_morph1 = morph_tps_wrapper(img_mask1,img_mask2,[faceShapeX(1,:)' faceShapeY(1,:)'],...
-    [faceShapeX(2,:)' faceShapeY(2,:)'],1,0);
-img_morph2 = morph_tps_wrapper(img_mask2,img_mask1,[faceShapeX(2,:)' faceShapeY(2,:)'],...
-    [faceShapeX(1,:)' faceShapeY(1,:)'],1,0);
+    [faceShapeX,faceShapeY] = findCoorPts(boxNose, boxMouth, boxEyeR, boxEyeL);
+    
+    if showAll
+        figure(); imshow(img);
+        hold on;
+        scatter(faceShapeX(:), faceShapeY(:));
+        hold off;
+    end
+    convHullX = faceShapeX(:,1:4);
+    convHullY = faceShapeY(:,1:4);
+    
+    img_mask1 = makeMask(img,convHullX(1,:),convHullY(1,:));
+    img_mask2 = makeMask(img,convHullX(2,:),convHullY(2,:));
+    
+    disp('MORPHING IMAGE');
+    
+    img_morph1 = morph_tps_wrapper(img_mask1,img_mask2,[faceShapeX(1,:)' faceShapeY(1,:)'],...
+        [faceShapeX(2,:)' faceShapeY(2,:)'],1,0);
+    img_morph2 = morph_tps_wrapper(img_mask2,img_mask1,[faceShapeX(2,:)' faceShapeY(2,:)'],...
+        [faceShapeX(1,:)' faceShapeY(1,:)'],1,0);
 catch
     img_frame = img;
     return
