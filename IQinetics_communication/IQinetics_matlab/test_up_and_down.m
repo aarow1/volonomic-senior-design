490d1790bb5b87946cbbdf8c314670f52f1477e4
Aaron Weinstein <aarow@seas.upenn.edu>
2017-02-08 19:33:32 -0500

    continued trying to tune motor gains

--- IQinetics_communication/IQinetics_matlab/test_up_and_down.m
+++ IQinetics_communication/IQinetics_matlab/test_up_and_down.m
@@ -1,13 +1,10 @@
 
-% mot.set('velocity_ff0', -0.019312895282361);
-% mot.set('velocity_ff1', 0.004648555991083);
-% mot.set('velocity_ff2', 0.000000121309706);
-
 mot.set('velocity_ff0', 0);
-mot.set('velocity_ff1', 0);
-mot.set('velocity_ff2', 0);
+mot.set('velocity_ff1', 0.004648555991083);
+mot.set('velocity_ff2', 0.000000121309706);
+
 mot.set('velocity_Kp', 0.03);
-mot.set('velocity_Ki', 0.005);
+mot.set('velocity_Ki', 0.0002);
 mot.set('velocity_Kd', 0.00);
 mot.save_all();
 pause(1);
@@ -15,18 +12,25 @@ pause(1);
 vels = zeros(400,1);
 ind = 1;
 
-num_tries = 8;
+num_tries = 6;
 
 tic
 for ii = 1:num_tries
-    spd = 200 * (ii/num_tries) * (-1)^(ii)
+    spd = 600 * (ii/num_tries) * (-1)^(ii)
     mot.set('cmd_velocity', spd);
-    while(toc < 1)
+    while(toc < 3)
         vels(ind) = mot.get('obs_velocity');
         ind = ind+1;
     end
     tic
 end
+
+%%
+filterPoints = 10;
+filtercoeffs = ones(1, filterPoints)/filterPoints;
+
+vels = filter(filtercoeffs, 1, vels);
+
 
 hold on;
 plot(vels);
