a935cae90d82174606bdb40e969d3a671b94359f
Aaron Weinstein <aarow@seas.upenn.edu>
2017-02-15 17:03:04 -0500

    Worked in B2 with the thrust stand. Didn't get much to change

--- IQinetics_communication/IQinetics_matlab/step.m
+++ IQinetics_communication/IQinetics_matlab/step.m
@@ -1,15 +1,15 @@
-target_spd = 1000;
+target_spd = -50;
 margin = .9;
 vels = [];
 ts = [];
-hold_time = 1.5;
+hold_time = 3;
 tic
 while toc < hold_time
     mot.set('cmd_velocity', target_spd);
     vels = [vels mot.get('obs_velocity')];
     ts = [ts toc];
     
-    if vels(end) < margin*target_spd
+    if abs(vels(end)) < abs(margin*target_spd)
         tic
     end
 end
