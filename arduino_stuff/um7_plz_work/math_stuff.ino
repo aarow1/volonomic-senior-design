a048be9e7181afc7fdf4551c085a86b2081a46e6
Aaron Weinstein <aarow@seas.upenn.edu>
2017-03-03 18:22:33 -0500

    got closer to making linear algebra stuff happen

---
+++ arduino_stuff/um7_plz_work/math_stuff.ino
@@ -0,0 +1,37 @@
+#include <BasicLinearAlgebra.h>
+
+template <typename T> int sign(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+void qmultiply(Matrix<4,1> a, Matrix<4,1> b, Matrix<4,1> c){
+  c(0) = b(0)*a(0) - b(1)*a(1) - b(2)*a(2) - b(3)*a(3);
+  c(1) = b(0)*a(1) + b(1)*a(0) - b(2)*a(3) + b(3)*a(2);
+  c(2) = b(0)*a(2) + b(1)*a(3) + b(2)*a(0) - b(3)*a(1);
+  c(3) = b(0)*a(3) - b(1)*a(2) + b(2)*a(1) + b(3)*a(0);
+};
+
+void qinverse(Matrix<4,1> q, Matrix<4,1> q_inv){
+  q_inv(0) = q(0);
+  q_inv(1) = -q(1);
+  q_inv(2) = -q(2);
+  q_inv(3) = -q(3);
+};
+
+// <3,1> cross(Matrix<3,1> a, Matrix<3,1> b){
+//  Matrix<3,1> c;
+//  c(0) = a(1)*b(2) - a(2)*b(1);
+//  c(1) = a(2)*b(0) - a(0)*b(2);
+//  c(2) = a(0)*b(1) - a(1)*b(0);
+//  return c;
+//};
+
+int scalar_multiply(double a, Matrix b){
+  Matrix<N,1> c;
+  for(int i = 0; i<N; i++){
+    c(i) = a*b(i);
+  }
+  return 10;
+//  return c;
+}
+
--- arduino_stuff/um7_plz_work/um7_plz_work.ino
+++ arduino_stuff/um7_plz_work/um7_plz_work.ino
@@ -1,12 +1,12 @@
-#include <Quaternion.h>
 #include <BasicLinearAlgebra.h>
 #include <UM7.h>
+
+#define Quaternion Matrix<4,1,double>
 
 //Connect the RX pin on the UM7 to TX1 (pin 18) on the DUE
 //Connect the TX pin on the UM7 to RX1 (pin 19) on the DUE
 
 UM7 imu;
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,8 +15,21 @@ void setup() {
   pinMode(13, OUTPUT);
 }
 
-Quaternion q_des = Quaternion();
+Quaternion q_curr;
+Quaternion q_curr_inv;
+double id[4][1] = {1, 0, 0, 0};
+Quaternion q_des(id);
 Quaternion q_err;
+
+Matrix<3, 1, double> w_ff;
+Matrix<3, 1, double> w_des;
+
+Matrix<3, 1, double> t_des;
+double tau_att = 1.0;
+double tau_w = 1.0;
+
+double J_vi_stupid[3][3] = {{1, 0, 0}, {0,1,0}, {0,0,1}};
+Matrix<3, 3, double> J_vi(J_vi_stupid);
 
 void loop() {
 
@@ -26,13 +39,24 @@ void loop() {
     }
   }
 
-  q_err = imu.q_att.inverse().multiply(q_des);
+  qinverse(q_curr, q_curr_inv);
+  qmultiply(q_curr_inv, q_des, q_err);
 
-  Serial.print("q_cur = ");
-  Serial.print(imu.q_att.toString());
-  Serial.print("q_des = ");
-  Serial.print(q_des.toString());
-  Serial.print("q_err = ");
-  Serial.println(q_err.toString());
+  w_des(0) = (2 / tau_att) * sign(q_err(0)) * q_err(1) + w_ff(0);
+  w_des(1) = (2 / tau_att) * sign(q_err(0)) * q_err(2) + w_ff(1);
+  w_des(2) = (2 / tau_att) * sign(q_err(0)) * q_err(3) + w_ff(2);
 
+//  t_des = scalar_multiply((1 / tau_w), J_vi * (w_des - imu.w)); + cross(imu.w, J_vi*imu.w);
+scalar_multiply(tau_att, w_des);
+
+  Serial.printf("q_cur = [%2.2f,\t%2.2f,\t%2.2f,\t%2.2f]\n",
+                imu.q_att(0), imu.q_att(1), imu.q_att(2), imu.q_att(3));
+  Serial.printf("w = [%2.2f,\t%2.2f,\t%2.2f]\n", imu.w(0), imu.w(1), imu.w(2));
+  //  Serial.println("q_des = " + q);
+  //    Serial.print(q_des.toString());
+  //    Serial.print("q_err = ");
+  //    Serial.print(q_err.toString());
+  //  Serial.printf("\tw_des = [%2.2f, %2.2f, %2.2f]\n", w_des(0), w_des(1), w_des(2));
 }
+
+
