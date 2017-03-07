f3006442c972b432ec375642b83701f5b4d98f6b
cherylliu <chl@seas.upenn.edu>
2017-03-07 07:56:04 -0700

    added some comments

--- arduino_stuff/readPkt/readPkt.ino
+++ arduino_stuff/readPkt/readPkt.ino
@@ -9,7 +9,7 @@ float forcelin_des[3];
 
 int pktIdx = 0;
 int state = 0;
-int i = 0; 
+int i = 0;
 enum {ATT_CURR, ATT_DES, OMEGADOT_DES, FORCELIN_DES};
 
 const int ledPin = 13;
@@ -18,6 +18,7 @@ void setup() {
   Serial.begin(9600); //USB
   Serial1.begin(9600); //XBee
   xbee.setSerial(Serial1);
+  
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   delay(500);
@@ -25,62 +26,66 @@ void setup() {
   Serial.println("teensy ready");
 }
 
+// union magic
 union {
   byte b[4];
   float f;
-} u;
+} u; 
 
 void loop() {
   if (Serial1.available() > 0) {
-    c = readpkt();
-    switch (state) {
-    case ATT_CURR:
-    att_curr[i] = c;
-    i++;
-    if (i >= 4) {
-      i = 0; 
-      state = ATT_DES;
-    }
-    case ATT_DES:
-    att_des[i] = c;
-    i++;
-    if (i >= 4) {
-      i = 0; 
-      state = OMEGADOT_DES;
-    }
-    case OMEGADOT_DES:
-    omegadot_des[i] = c;
-    i++;
-    if (i >= 3) {
-      i = 0; 
-      state = FORCELIN_DES;
-    }
-    case FORCELIN_DES:
-    forcelin_des[i] = c;
-    i++;
-    if (i >= 3) {
-      i = 0; 
-      continue; 
-    }
-    }
+    state = ATT_CURR; i = 0;
+    while (!readpkt()) {}
   }
 }
 
-float readpkt() {
+bool readpkt() {
   while (pktIdx < 4) {
-      u.b[pktIdx] = Serial1.read();
-      pktIdx++;
-    }
-    pktIdx = 0;
-    return u.f;
+    u.b[pktIdx] = Serial1.read();
+    pktIdx++;
+  }
+  pktIdx = 0;
+  switch (state) {
+    case ATT_CURR:
+      att_curr[i] = u.f;
+      i++;
+      if (i >= 4) {
+        i = 0;
+        state = ATT_DES;
+      }
+      return 0;
+
+    case ATT_DES:
+      att_des[i] = u.f;
+      i++;
+      if (i >= 4) {
+        i = 0;
+        state = OMEGADOT_DES;
+      }
+      return 0;
+
+    case OMEGADOT_DES:
+      omegadot_des[i] = u.f;
+      i++;
+      if (i >= 3) {
+        i = 0;
+        state = FORCELIN_DES;
+      }
+      return 0;
+
+    case FORCELIN_DES:
+      forcelin_des[i] = u.f;
+      i++;
+      if (i >= 3) {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    default:
+      return 0;
+  }
+
 }
-//int i;
-//void bin2float(byte pktIn[4]) {
-//  for ( i = 0 ; i < 4 ; i++ )  {
-//    u.b[i] = pktIn[i] ;
-//  }
-//  //  u.b = pktIn;
-//  Serial.println(u.f) ;
-//}
 
 
