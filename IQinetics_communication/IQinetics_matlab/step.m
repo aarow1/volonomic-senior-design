67024248c2a51b7288faa5b7b46d5d19eca3e026
Daniel Shanks <shanksd@seas.upenn.edu>
2017-03-19 16:15:04 -0400

    sys control matlab updates

--- IQinetics_communication/IQinetics_matlab/step.m
+++ IQinetics_communication/IQinetics_matlab/step.m
@@ -1,41 +1,69 @@
 
-target_spd = 1000;
+target_spd = 1700;
 margin = .9;
 vels = [];
 close all
 ts = [];
-
+kv = 2109;
+kt = 1/(kv/60 * 2*pi);
 pwms = [];
 vs = [];
-currents = [];
-hold_time = 2.5;
+currents = 0;
+if ~exist('I_bus','var')
+    I_bus = 0;% mA read from Ammeter
+end
 
+hold_time = 5;
+
+a = 1;
 tic
 while toc < hold_time
-    mot.set('cmd_velocity', target_spd);
-    vels = [vels mot.get('obs_velocity')];
-    pwms = [pwms mot.get('drive_pwm')];
-    currents = [currents mot.get('est_motor_amps')];
-    vs = [vs mot.get('obs_supply_volts')];
+    mot0.set('cmd_velocity', target_spd);
+    vels = [vels mot0.get('obs_velocity')];
+    pwms = [pwms mot0.get('drive_pwm')];
+    c = mot0.get('est_motor_amps');
+    currents = [currents c*a+(1-a)*currents(end)];
+    
+    vs = [vs mot0.get('obs_supply_volts')];
     ts = [ts toc];
     
 %     if abs(vels(end)) < abs(margin*target_spd)
 %         tic
 %     end
 end
-subplot(2,2,1)
+currents(1) = [];
+est_T = currents*kt;
+est_P = est_T.*vels;
+ss = abs(vels)>abs(margin*target_spd);% steady state values
+i = input('Enter I bus in Amps (if it''s different from prev)');
+if ~isempty(i)
+    I_bus = i;
+end
+est_P_in = vs*I_bus;
+eff = est_P./est_P_in;
+ave_eff = mean(eff(ss));
+disp(['Average efficiency in steady state: ' num2str(ave_eff)])
+subplot(2,3,1)
 plot(ts,vels)
 title('velocity')
 grid on
-subplot(2,2,2);
+subplot(2,3,2);
 plot(ts,pwms)
 title('pwm')
 grid on
-subplot(2,2,3);
+subplot(2,3,3);
 plot(ts,currents)
 title('current')
 grid on
-subplot(2,2,4);
+subplot(2,3,4);
 plot(ts,vs)
 title('volts')
+grid on
+subplot(2,3,5);
+plot(ts,est_P)
+title('Power Out')
+grid on
+subplot(2,3,6);
+plot(ts,eff)
+title('Efficiency')
 grid on
